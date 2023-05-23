9e858f229aa28854872ecd4e7747cacad61bb4ca
Lumi <shaicloud@gmail.com>
2023-05-23 04:09:45 -0400

    rewrote checkRoundWinner logic

--- src/modules/checkRoundWinner.ts
+++ src/modules/checkRoundWinner.ts
@@ -1,13 +1,14 @@
 export default function checkRoundWinner(playerChoice: string, computerChoice: string): string {
+	const rules: Record<string, string> = {
+		rock: "scissors",
+		paper: "rock",
+		scissors: "paper",
+	};
+
 	if (playerChoice === computerChoice) {
 		return "draw";
-	} else if (
-		(playerChoice === "rock" && computerChoice === "scissors") ||
-		(playerChoice === "paper" && computerChoice === "rock") ||
-		(playerChoice === "scissors" && computerChoice === "paper")
-	) {
+	} else if (rules[playerChoice] === computerChoice) {
 		return "Player Wins!";
-	} else {
-		return "Computer Wins!";
 	}
+	return "Computer Wins!";
 }
